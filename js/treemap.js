d11c1f5d358409e46bece775c86dd1dee2110098
esaugtz <dgesau@gmail.com>
2016-09-01 15:25:47 -0500

    Cambiando redondeo en porcentajes

--- js/treemap.js
+++ js/treemap.js
@@ -71,7 +71,7 @@ $(document).ready(function(){
           return formatted;
         }
         else {
-          return formattedFirst;
+          return formatted;
         }
       },
       "locale":"es_ES"
